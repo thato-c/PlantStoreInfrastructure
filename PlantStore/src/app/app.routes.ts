44122aa2815e2d43b626e8e9fa79040ecd95c600
Thato <t.chitja369@gmail.com>
2025-11-17 11:28:51 +0200

    Add Blog Post component and styling to it

--- PlantStore/src/app/app.routes.ts
+++ PlantStore/src/app/app.routes.ts
@@ -8,4 +8,5 @@ export const routes: Routes = [
 
     // Blog (SSG)
     {path: 'blog', loadComponent: () => import('./blog/blog-list/blog-list.component').then(m => m.BlogListComponent)},
+    {path: 'blog/:id', loadComponent: () => import('./blog/blog-post/blog-post.component').then(m => m.BlogPostComponent)}
 ];
--- PlantStore/src/app/blog/blog-post/blog-post.component.ts
+++ PlantStore/src/app/blog/blog-post/blog-post.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { BlogPost } from '../../models/blog.model';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.css'
 })
-export class BlogPostComponent {
+export class BlogPostComponent implements OnInit{
+  post: BlogPost | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ){}
+
+  ngOnInit(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.post = this.blogService.getPostsById(id);
+  }
 }
